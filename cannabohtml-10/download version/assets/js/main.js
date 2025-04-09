d5945333136cbd0af07757fd49d5cecf42496ebe
HatimmD <hdaanoun2014@gmail.com>
2025-04-09 12:04:36 +0100

    Supprimer la clé API en dur et utiliser une variable d'environnement

--- cannabohtml-10/download version/assets/js/main.js
+++ cannabohtml-10/download version/assets/js/main.js
@@ -972,8 +972,10 @@
     ".searchClose",
     "show"
   );
-  const API_KEY = "12345-FAKE-KEY-SECRET"; // Exemple de vulnérabilité
-  console.log(API_KEY);
+ // const API_KEY = "12345-FAKE-KEY-SECRET"; // Exemple de vulnérabilité
+  //console.log(API_KEY);
+  const API_KEY = process.env.API_KEY;  // Remplacer la clé en dur
+
   
   // End
 })(jQuery);
